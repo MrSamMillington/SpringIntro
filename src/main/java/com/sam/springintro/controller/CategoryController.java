4f05f5c948df57735efff24ccca3ea891f6a3792
Sam <s.j.millington@hotmail.co.uk>
2018-01-08 09:32:57 +0000

    First commit

---
+++ src/main/java/com/sam/springintro/controller/CategoryController.java
@@ -0,0 +1,41 @@
+package com.sam.springintro.controller;
+
+import com.sam.springintro.Data.CategoryRepository;
+import com.sam.springintro.Data.GifRepository;
+import com.sam.springintro.model.Category;
+import com.sam.springintro.model.Gif;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class CategoryController {
+
+    @Autowired
+    private GifRepository gifRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @RequestMapping("/categories")
+    public String categories(ModelMap modelMap){
+        List<Category> allCategories = categoryRepository.getAllCategories();
+        modelMap.put("categories", allCategories);
+        return "categories";
+    }
+
+    @RequestMapping("/category/{id}")
+    public String category(@PathVariable int id, ModelMap modelMap){
+        Category category = categoryRepository.findById(id);
+        modelMap.put("category", category);
+
+        List<Gif> gifs = gifRepository.findByCategoryId(id);
+        modelMap.put("gifs", gifs);
+
+        return "category";
+    }
+}
---
+++ src/main/java/com/sam/springintro/controller/GifController.java
@@ -0,0 +1,41 @@
+package com.sam.springintro.controller;
+
+import com.sam.springintro.Data.CategoryRepository;
+import com.sam.springintro.Data.GifRepository;
+import com.sam.springintro.model.Category;
+import com.sam.springintro.model.Gif;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Controller
+public class GifController {
+
+    @Autowired //dependency injection (di)
+    private GifRepository gifRepository;
+
+
+
+    @RequestMapping("/")
+    public String listGifs(ModelMap modelMap){
+        List<Gif> allGifs = gifRepository.getAllGifs();
+        modelMap.put("gifs", allGifs);
+        return "home";
+    }
+
+    @RequestMapping("/gif/{name}")
+    public String gifDetails(@PathVariable String name, ModelMap modelMap){
+        Gif gif = gifRepository.findByName(name);
+        modelMap.put("gif", gif);
+        return "gif-details";
+    }
+
+
+
+
+}
